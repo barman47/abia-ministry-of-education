070c074395cfad6ba2ef324245009facb0873bed
barman47 <nomsouzoanya@yahoo.co.uk>
2020-08-17 09:44:13 +0100

    Initial Commit

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const port = process.env.port || 5000;
 const publicPath = path.resolve(__dirname, 'client', 'public', 'build');
 
+app.use(express.static(publicPath));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(publicPath, 'index.html'));
 });
