3553bdfbe96bf948813b70d32cff8d58e0a4d39f
barman47 <nomsouzoanya@yahoo.co.uk>
2020-08-17 10:05:01 +0100

    Initial Commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const publicPath = path.resolve(__dirname, 'client', 'public', 'build');
 
 app.use(express.static(publicPath));
